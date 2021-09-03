00d162a4cd1ac8369965c12767192558b3fb6aad
Arsh Khatwani <arsh.khatwani@gmail.com>
2021-09-03 14:51:16 +0530

    React router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  )
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <div>
+            <h1>hello world</h1>
+          </div>
+        </Route>
+        <Route path="/about" exact>
+          <div>
+            <h1>About Page</h1>
+          </div>
+        </Route>
+        <Route path="/contact" exact>
+          <div>
+            <h1>Contact Page</h1>
+          </div>
+        </Route>
+        <h1>404 not found</h1>
+      </Switch>
+    </Router>
+  );
 }
