6bb6a7c2d92ae4ac0036d3c911248466ab42b22f
Arsh Khatwani <arsh.khatwani@gmail.com>
2021-10-09 20:22:02 +0530

    Order box added

---
+++ client/src/components/OrderBox.jsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+import { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Paper from "@mui/material/Paper";
+import Draggable from "react-draggable";
+import { Typography, Button, Box, TextField } from "@material-ui/core";
+
+function PaperComponent(props) {
+  return (
+    <Draggable
+      handle="#draggable-dialog-title"
+      cancel={'[class*="MuiDialogContent-root"]'}
+    >
+      <Paper {...props} />
+    </Draggable>
+  );
+}
+
+export default function OrderBox(props) {
+  const { open, setOpen, title, ltp, type } = props;
+
+  const btnColor = type === "buy" ? "primary" : "secondary";
+  const themeColor = type === "buy" ? "#3f51b5" : "#ff3d00";
+
+  const [price, setPrice] = useState(ltp);
+  const [qty, setQty] = useState(1);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperComponent={PaperComponent}
+        aria-labelledby="draggable-dialog-title"
+      >
+        <DialogTitle
+          style={{ cursor: "move" }}
+          id="draggable-dialog-title"
+          style={{ backgroundColor: themeColor, color: "#fff" }}
+        >
+          {title} x {qty} Qty
+          <Typography>LTP: {ltp}</Typography>
+        </DialogTitle>
+        <DialogContent>
+          <Box
+            margin="10px 0"
+            padding="1rem"
+            display="flex"
+            justifyContent="space-around"
+            alignItems="space-around"
+            width="100%"
+            marginBottom="5px"
+          >
+            <TextField
+              variant="outlined"
+              label="Qty."
+              type="number"
+              color={btnColor}
+              size="small"
+              style={{ width: "25%" }}
+              focused
+              value={qty}
+              onChange={(e) => setQty(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              label="Price"
+              type="number"
+              color={btnColor}
+              size="small"
+              style={{ width: "40%" }}
+              focused
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </Box>
+          <Box>
+            <Typography style={{ float: "left" }}>
+              Margin: ${price * qty}
+            </Typography>
+          </Box>
+        </DialogContent>
+        <hr style={{ width: "100%" }} />
+        <DialogActions>
+          <Button
+            autoFocus
+            onClick={handleClose}
+            variant="outlined"
+            disableElevation
+            color={btnColor}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            disableElevation
+            color={btnColor}
+            onClick={handleClose}
+          >
+            Execute
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- client/src/components/StockTab.jsx
+++ client/src/components/StockTab.jsx
@@ -1,46 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography, Button } from "@material-ui/core";
+import OrderBox from "./OrderBox";
 
 export default function StockTab(props) {
   const { symbol, ltp } = props;
 
+  const [openB, setOpenB] = useState(false);
+  const [openS, setOpenS] = useState(false);
+
   return (
-    <Box
-      marginBottom="10px"
-      paddingY="6px"
-      padding="6px"
-      paddingX="6px"
-      width="98%"
-      boxShadow={1}
-      display="flex"
-      justifyContent="space-around"
-    >
-      <Box>
-        <Typography>{symbol}</Typography>
+    <>
+      <OrderBox
+        open={openB}
+        setOpen={setOpenB}
+        title={`Buy ${symbol}`}
+        ltp={ltp}
+        type="buy"
+      />
+      <OrderBox
+        open={openS}
+        setOpen={setOpenS}
+        title={`Sell ${symbol}`}
+        ltp={ltp}
+        type="sell"
+      />
+      <Box
+        marginBottom="10px"
+        paddingY="6px"
+        padding="6px"
+        paddingX="6px"
+        width="98%"
+        boxShadow={1}
+        display="flex"
+        justifyContent="space-around"
+      >
+        <Box>
+          <Typography>{symbol}</Typography>
+        </Box>
+        <Box>
+          <Button
+            variant="contained"
+            size="small"
+            disableElevation
+            style={{ margin: "0 0.1rem" }}
+            color="primary"
+            onClick={() => {
+              setOpenB(true);
+            }}
+          >
+            B
+          </Button>
+          <Button
+            variant="contained"
+            size="small"
+            disableElevation
+            style={{ margin: "0 0.1rem" }}
+            color="secondary"
+            onClick={() => {
+              setOpenS(true);
+            }}
+          >
+            S
+          </Button>
+        </Box>
+        <Box boxSizing="border-box">
+          <Typography variant="subtitle2">{ltp}</Typography>
+        </Box>
       </Box>
-      <Box>
-        <Button
-          variant="contained"
-          size="small"
-          disableElevation
-          style={{ margin: "0 0.1rem" }}
-          color="primary"
-        >
-          B
-        </Button>
-        <Button
-          variant="contained"
-          size="small"
-          disableElevation
-          style={{ margin: "0 0.1rem" }}
-          color="secondary"
-        >
-          S
-        </Button>
-      </Box>
-      <Box boxSizing="border-box">
-        <Typography variant="subtitle2">{ltp}</Typography>
-      </Box>
-    </Box>
+    </>
   );
 }
