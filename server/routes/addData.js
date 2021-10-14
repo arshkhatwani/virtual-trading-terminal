7daf56899b4283e7018d1087dd4762025675273b
Arsh Khatwani <arsh.khatwani@gmail.com>
2021-10-14 15:31:55 +0530

    Stock sell API done

--- server/routes/addData.js
+++ server/routes/addData.js
@@ -57,18 +57,25 @@ router.post("/user/stock/buy", decodeToken, async (req, res) => {
       avgPrice = avgPrice.toFixed(2);
       const totalQty = qty + qty2;
 
-      const updatedPos = await userPositions.findOneAndUpdate(
-        { uid, stock },
-        { price: avgPrice, qty: totalQty },
-        { new: true }
-      );
-
       // Modifying funds
       const modifyFunds = await userModel.findOneAndUpdate(
         { uid },
         { funds },
         { new: true }
       );
+
+      // Checking position square-off
+      if (totalQty == 0) {
+        const deletePos = await userPositions.findOneAndDelete({ uid, stock });
+        return res.status(200).json({ position: "cleared", newFunds: funds });
+      }
+
+      const updatedPos = await userPositions.findOneAndUpdate(
+        { uid, stock },
+        { price: avgPrice, qty: totalQty },
+        { new: true }
+      );
+
       return res
         .status(200)
         .json({ posInfo: updatedPos, newFunds: modifyFunds.funds });
@@ -77,6 +84,81 @@ router.post("/user/stock/buy", decodeToken, async (req, res) => {
     // If no position exists then creating new
     req.body.uid = uid;
 
+    const newData = new userPositions(req.body);
+    const savedData = await newData.save();
+
+    // Modifying funds
+    const modifyFunds = await userModel.findOneAndUpdate(
+      { uid },
+      { funds },
+      { new: true }
+    );
+
+    return res
+      .status(200)
+      .json({ posInfo: savedData, newFunds: modifyFunds.funds });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+// Stock sell route
+router.post("/user/stock/sell", decodeToken, async (req, res) => {
+  try {
+    const { uid } = req.headers.tokenData;
+
+    var funds = await userModel.findOne({ uid }).select({ funds: 1 });
+    funds = funds.funds;
+    var { price, qty, stock } = req.body;
+    funds = funds + price * qty;
+
+    // Checking if position already exists
+    const checkPos = await userPositions.findOne({ uid, stock });
+    if (checkPos != null) {
+      qty *= -1;
+      const qty2 = checkPos.qty;
+      const price2 = checkPos.price;
+      var avgPrice = (qty2 * price2 + qty * price) / (qty + qty2);
+      avgPrice = avgPrice.toFixed(2);
+      const totalQty = qty + qty2;
+
+      // Modify funds
+      const modifyFunds = await userModel.findOneAndUpdate(
+        { uid },
+        { funds },
+        { new: true }
+      );
+
+      // Checking position square-off
+      if (totalQty == 0) {
+        const deletePos = await userPositions.findOneAndDelete({ uid, stock });
+        return res.status(200).json({ position: "cleared", newFunds: funds });
+      }
+
+      if (totalQty > 0) {
+        avgPrice = price2;
+      }
+
+      if (qty2 < Math.abs(qty)) {
+        avgPrice = price;
+      }
+
+      // Update position
+      const updatedPos = await userPositions.findOneAndUpdate(
+        { uid, stock },
+        { price: avgPrice, qty: totalQty },
+        { new: true }
+      );
+
+      return res
+        .status(200)
+        .json({ posInfo: updatedPos, newFunds: modifyFunds.funds });
+    }
+
+    // If no position exists then creating new
+    req.body.uid = uid;
+    req.body.qty *= -1;
     const newData = new userPositions(req.body);
     const savedData = await newData.save();
 
